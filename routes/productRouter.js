5e91f99ba2cb31d8407a90d80d1ea6798342acda
YuriyEG <you@example.com>
2024-02-10 06:15:27 +0300

    sf

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -6,6 +6,7 @@ router.get("/", productController.getAll);
 // router.post("/registration", productController.registration);
 // router.post("/login", productController.login);
 // router.get("/auth", productController.check);
+router.get("/create", productController.create);
 
 router.post("/create", productController.create);
 router.delete("/:id", productController.delete);
