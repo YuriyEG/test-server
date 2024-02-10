7e70e2bf24d325ccdeffb285e873b608fbed3e0d
YuriyEG <you@example.com>
2024-02-10 06:56:30 +0300

    sfds

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,12 @@ const { Type } = require("../models/models");
 
 class ProductController {
   async getAll(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept,recording-session"
+    );
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET");
     const types = await Type.findAll();
     return res.json(types);
   }
@@ -13,13 +19,13 @@ class ProductController {
   async create(req, res) {
     const { name } = req.body;
     const type = await Type.create({ name });
-    return res.json(Type);
+    return res.json(type);
   }
-  // async delete(req, res) {
-  //   const { id } = req.query;
-  //   const type = await Type.findOne(id);
-  //   return res.json(type);
-  // }
+  async delete(req, res) {
+    const { id } = req.query;
+    const type = await Type.findOne(id);
+    return res.json(type);
+  }
 }
 
 module.exports = new ProductController();
