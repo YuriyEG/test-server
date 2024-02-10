3f841aaf545d0245330391b6461581daaa219ddd
YuriyEG <you@example.com>
2024-02-10 09:18:47 +0300

    sfds

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://localhost:5173/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
 });
 
 app.use(express.json());
