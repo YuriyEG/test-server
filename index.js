d3073237f25a3b3ad06253ddbfc927ff28df3934
YuriyEG <you@example.com>
2024-02-10 09:21:57 +0300

    sfs

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
 });
 
 app.use(express.json());
