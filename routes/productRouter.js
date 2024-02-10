109caf658663aec105ef8339ad64876d567653e4
YuriyEG <you@example.com>
2024-02-10 04:06:58 +0300

    pos

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,12 +2,12 @@ const Router = require("express");
 const router = new Router();
 const productController = require("../controllers/productController");
 
-router.get("/", productController.getAll);
+router.post("/", productController.getAll);
 // router.post("/registration", productController.registration);
 // router.post("/login", productController.login);
 // router.get("/auth", productController.check);
 
-router.post("/", productController.create);
+// router.post("/", productController.create);
 router.delete("/:id", productController.delete);
 
 module.exports = router;
