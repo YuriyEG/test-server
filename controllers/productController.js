f368926aef979c69291dd4a0325d74c8da4e5d4e
YuriyEG <you@example.com>
2024-02-10 03:42:13 +0300

    headers added to getAll products

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,12 @@ const { Type } = require("../models/models");
 
 class ProductController {
   async getAll(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept,recording-session"
+    );
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET");
     const types = await Type.findAll();
     return res.json(types);
   }
