8388ad49901ef96fb2feeae96d61a44e20c6c590
YuriyEG <you@example.com>
2024-02-10 07:53:44 +0300

    sfds

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,recording-session"
+  );
+  res.header("Access-Control-Allow-Methods", "DELETE", "POST", "GET");
+  next();
+});
+
 app.use(express.json());
 app.use("/api", router);
 app.use("/test", (req, res) => {
