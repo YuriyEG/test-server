70397130b0285c02dae9100096c474dffe51e984
YuriyEG <you@example.com>
2024-02-10 05:57:30 +0300

    sfs

--- controllers/productController.js
+++ controllers/productController.js
@@ -17,10 +17,9 @@ class ProductController {
   //   res.json({ check: "check" });
   // }
   async create(req, res) {
-    const { name, producer, about } = req.body;
-    // const type = await Type.create({ name, producer, about });
-    return res.json({ name, producer, about });
-    // return res.json({ result: true });
+    const { name } = req.body;
+    const type = await Type.create({ name });
+    return res.json(type);
   }
   async delete(req, res) {
     const { id } = req.query;
