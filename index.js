eb5f463295b8cda7b2ddc8a82c91fa3f2b5e23b7
YuriyEG <you@example.com>
2024-02-10 09:11:27 +0300

    sdfs

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://localhost:5173/");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,recording-session"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "DELETE", "POST", "GET");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 });
 
