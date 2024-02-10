d59fa0e9f18d1492d03292a94e0e6d1aebdf43ef
YuriyEG <you@example.com>
2024-02-10 10:32:21 +0300

    sd

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    // "Origin, X-Requested-With, Content-Type, Accept",
+    "Content-Type"
   );
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
